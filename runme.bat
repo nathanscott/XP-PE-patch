8d6be6560f60f16b06f759e326c3566790a31611
Nathan <nathansampimon@Nathans-iMac.local>
2009-09-02 19:42:26 +1000

    make it nicer

--- runme.bat
+++ runme.bat
@@ -3,6 +3,6 @@ ECHO OFF;
 
 md "C:\Program Files\Common Files\VGX\";
 
-copy vgx.dll "C:\Program Files\Common Files\VGX\";
+copy DLLs\vgx.dll "C:\Program Files\Common Files\VGX\";
 
 regsvr32 "C:\Program Files\Common Files\VGX\vgx.dll";
